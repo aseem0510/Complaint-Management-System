8d274539e4bdcda9342ed96f63aca52a91465b42
Aseem <aseemranjan10@gmail.com>
2021-12-29 01:43:42 +0530

    Password Change Complete

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
+
+from django.contrib import messages
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -6,4 +10,33 @@ def dashboard(request):
     return render(request, 'catalog/dashboard.html')
 
 def changePassword(request):
-    return render(request, 'catalog/changePassword.html')
+    
+    if request.method == "POST":
+        
+        username = request.POST.get("username")
+        currPassword = request.POST.get("currPassword")
+        newPassword = request.POST.get("newPassword")
+        confPassword = request.POST.get("confPassword")
+        
+        
+        from django.contrib.auth import authenticate
+        user = authenticate(username=username, password=currPassword)
+
+        if user is not None:
+                
+            if newPassword == confPassword:
+                
+                user.set_password(newPassword)
+                user.save()
+                
+                messages.success(request, "Password Change !!")
+                return redirect("/catalog/changePassword")
+            
+            else:
+                print("Passwords doesn't match")
+        
+        else:
+            print("Wrong Current Password")
+    
+    else:
+        return render(request, 'catalog/changePassword.html')
